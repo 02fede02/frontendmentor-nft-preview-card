89539dc7db79490b043973cae75a02d8982fcdab
02fede02 <fede.birman93@gmail.com>
2022-10-19 17:59:30 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        softBlue: 'hsl(215, 51%, 70%)',
+        cyan: 'hsl(178, 100%, 50%)',
+        veryDarkBlueMainBg: 'hsl(217, 54%, 11%)',
+        veryDarkBlueCardBg: 'hsl(216, 50%, 16%)',
+        veryDarkBlueLine: 'hsl(215, 32%, 27%)',
+      },
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
